86bc41e14d79cf0778af0b3a6161f0860abd55f9
salman141 <62141231+salman141@users.noreply.github.com>
2020-09-01 16:31:23 +0500

    Updates

---
+++ precache-manifest.72fe2e94e6a608f9d296ba63dd4c9397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58c43deb7f5fedbd7edd52d9a840e01",
+    "url": "/slotmachinegame/index.html"
+  },
+  {
+    "revision": "6c4ef35ebaefde6ce918",
+    "url": "/slotmachinegame/static/css/main.bdf3130d.chunk.css"
+  },
+  {
+    "revision": "55f1519b3c7d3ebc8c64",
+    "url": "/slotmachinegame/static/js/2.a852dd50.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/slotmachinegame/static/js/2.a852dd50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c4ef35ebaefde6ce918",
+    "url": "/slotmachinegame/static/js/main.a5a32e31.chunk.js"
+  },
+  {
+    "revision": "ccb2185753bf71ddc326",
+    "url": "/slotmachinegame/static/js/runtime-main.125eab56.js"
+  }
+]);
